a426499fde4e8eab888127b96b9bed829c5ac9c5
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-10-26 10:31:47 -0300

    update

---
+++ JAVASCRIPT/Elementos Dom/Manipulando Elementos/script.js
@@ -0,0 +1,20 @@
+function criarElementoInnerText(){
+
+  const ul = document.getElementById("lista-de-elementos")
+
+  const novaLi = document.createElement("li");
+  novaLi.innerText = "Novo input com innerText";
+
+  const novoInput = document.createElement("input");
+
+  novali.appendChild(novoInput)
+
+  ul.appendChild(novaLi);
+
+}
+
+// innertext - texto - estilização css: elementos ocultos
+
+// textContext - texto
+
+// innerHtml - xonteudo htmml e texto
