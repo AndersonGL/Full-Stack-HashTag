66e2a6259afb48002240a23e5a47c6b63f4a26f0
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-10-26 16:44:35 -0300

    upgrade

--- JAVASCRIPT/Elementos Dom/Manipulando Elementos/script.js
+++ JAVASCRIPT/Elementos Dom/Manipulando Elementos/script.js
@@ -1,56 +1,34 @@
-function criarElementoInnerText() {
+  function criarElementoInnerText() {
 
   const ul = document.getElementById("lista-de-elementos")
 
   const novaLi = document.createElement("li");
-  novaLi.innerText = "Anderson Gouveia Lignelli";
+  novaLi.innerText = "Anderson Gouveia Lignelli ";
 
 
   const novoInput = document.createElement("input");
-
-
   novaLi.appendChild(novoInput)
-
   ul.appendChild(novaLi);
 
 }
 
 function criarElementoInnerHtml() {
+    const ul = document.getElementById("lista-de-elementos");
+    const novaLi = document.createElement("li");
+
+  novaLi.innerHTML = '<input type = "text" />';
+  ul.appendChild(novaLi); 
+}
+
+function criarElementoTextContent() {
 
   const ul = document.getElementById("lista-de-elementos");
   const novaLi = document.createElement("li");
-
-console.log("Adicionando ao clicar")  <--
-
-
-
-
-
-
-  // Continua
-
-
-  //    function criarElementoInnerHtml() {
-  //      const ul = document.getElementById("lista-de-elementos");
-  //        const novaLi = document.createElement("li");
-  //       novaLi.innerHTML = "Novo input com innerHTML <input type='text' />"; // Adicionando input direto no innerHTML
-  //         ul.appendChild(novaLi);
-  //      }
-
-  //      function criarElementoTextContent() {
-  //        const ul = document.getElementById("lista-de-elementos");
-  //         const novaLi = document.createElement("li");
-  //novaLi.textContent = "Novo input com textContent"; // Usando textContent
-  //      const novoInput = document.createElement("input");
-  //          novaLi.appendChild(novoInput); // Adicionando o input
-  //    ul.appendChild(novaLi);
-  //       }
-
-
-
-
-
-
+  const novoInput = document.createElement("input");
+  novaLi.textContent= "Atribuindo conteudo"
+  novaLi.appendChild(novoInput); 
+  ul.appendChild(novaLi); 
+   
 
 
 
