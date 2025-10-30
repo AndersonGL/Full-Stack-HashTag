8f7a851ebc7e38f705cd063512d4478bda6832fe
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-10-30 10:58:39 -0300

    upadte

--- JAVASCRIPT/Elementos Dom/Modificando Atributos/aula20.js
+++ JAVASCRIPT/Elementos Dom/Modificando Atributos/aula20.js
@@ -1,31 +1,38 @@
-function changeImage() {
-  const img = document.getElementById("myImage");
-  const currentSrc = img.getAttribute("src");
+//  getAttribute - Obter o valor do atributo
+//  setAttribute - Modificar o valor do atributo
 
-  if (currentSrc === "imagem1.png") {
-    img.setAttribute("src", "imagem2.png");
-    img.setAttribute("alt", "Professor Daniel");
-  } else {
+
+function changeImage() { 
+  const img =  document.getElementById("myImage");
+  const currentSrc = img.getAttribute("src");  // Pega o valor do atributo src
+  if (currentSrc === "imagem1.png") {         
+    img.setAttribute("src", "imagem2.png");  // Modifica o valor do atributo src
+     img.setAttribute("alt", "Professor Daniel"); //Modifica o valor do atributo alt
+  }else {
     img.setAttribute("src", "imagem1.png");
-    img.setAttribute("alt", "Silhueta de uma pessoa");
+    img.setAttribute("alt", "Sinueta de uma pessoa");
+
   }
 }
 
-function changeType() {
-  const input = document.getElementsByTagName("input");
+function  changeType() {
+  const input = document.getElementsByTagName("input")[0];
   const currentType = input.getAttribute("type");
 
   switch (currentType) {
+
     case "text":
-      input.setAttribute("type", "number");
+     input.setAttribute("type", "number");
       break;
-    case "number":
-      input.setAttribute("type", "radio");
-      break;
-    case "radio":
-      input.setAttribute("type", "range");
-      break;
-    default:
-      input.setAttribute("type", "text");
-  }
+     case "number":
+     input.setAttribute("type", "radio");
+     break;
+     case "radio":
+     input.setAttribute("type", "range");
+     break;
+      default:
+     input.setAttribute("type", "text");
+
+     
+}
 }
