aac1efb1f291741d0601b63f9b68d37b9c9fee17
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-11-17 16:37:13 -0300

    update

--- JAVASCRIPT/Formularios 123/ValidacaoForms-pt2/script.js
+++ JAVASCRIPT/Formularios 123/ValidacaoForms-pt2/script.js
@@ -1,32 +1,42 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const formulario  = document.getElementById("meuFormulario");
-  
-  formulario.addEventListener("submit", function(event) {
-     if (!validarCamposObrigatorios()) {
-      event.preventDefault();  // Impede o envio do formulário se houver campos obrigatórios não preenchidos
+document.addEventListener("DOMContentLoaded", function () {
+  const formulario = document.getElementById("meuFormulario");
 
-     }
+  formulario.addEventListener("submit", function (event) {
+    if (!validarCamposObrigatorios()) {
+      event.preventDefault(); // Impede o envio do formulário se houver campos obrigatórios não preenchidos
+    }
+    if (!compararValores()) {
+      event.preventDefault(); // Impede o envio do formulário se os valores não coincidirem
+    }
+  });
 
-  // event.preventDefault(); 
-    function validarCamposObrigatorios() {
-      let camposObrigatorios = document.querySelectorAll(".Obrigatorio");
+  function validarCamposObrigatorios() {
+    let camposObrigatorios = document.querySelectorAll(".Obrigatorio");
     //  console.log(camposObrigatorios);
 
-      let camposValidos = true;
+    let camposValidos = true;
 
-      for (let i = 0; camposObrigatorios.length > i; i++) {
-        let campo = camposObrigatorios[i];
+    for (let i = 0; camposObrigatorios.length > i; i++) {
+      let campo = camposObrigatorios[i];
 
-        if (campo.value === "" || campo.value === null) {
-          console.log(`O campo ${campo.name} é obrigatório.`);
-          camposValidos = false;
-
-        }
-    } // 0,1,2,3
-    
-      return camposValidos;
+      if (campo.value === "" || campo.value === null) {
+        console.log(`O campo ${campo.name} é obrigatório.`);
+        camposValidos = false;
+      }
+    }
+    return camposValidos;
   }
-   
 
-  });
+  function compararValores() {
+    const senha = document.getElementById("senha");
+    const confirmarSenha = document.getElementById("confirmarSenha");
+
+    if (senha.value !== confirmarSenha.value) {
+      console.log("As senhas não coincidem.");
+      return false;
+    }
+      return true;
+
+
+  }
 });
