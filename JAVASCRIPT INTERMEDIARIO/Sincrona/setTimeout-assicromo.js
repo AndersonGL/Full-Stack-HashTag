303c6ad3912087156a3b1da179b0660479f55d81
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-11-24 17:58:47 -0300

    Update setTimeout-assicromo.js

--- JAVASCRIPT INTERMEDIARIO/Sincrona/setTimeout-assicromo.js
+++ JAVASCRIPT INTERMEDIARIO/Sincrona/setTimeout-assicromo.js
@@ -2,7 +2,14 @@ console.log("inicio do programa");
 
 setTimeout(function () {
 console.log("Ola mundo assicronomo");
-},10000)
+},2000)
+
+console.log("Aguardaando 1 seg");
+console.log("Aguardando  2 seg");
+
+setTimeout(function () {
+console.log("Primeiro setTimeOut");
+},1000)
 
 
 console.log("termino do programa");
