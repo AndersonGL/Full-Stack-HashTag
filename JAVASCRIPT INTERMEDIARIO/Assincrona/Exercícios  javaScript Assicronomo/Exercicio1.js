d2e016d294458cadc4eae219bbed07a5cb3e81a8
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-12-11 19:24:44 -0300

    update

--- JAVASCRIPT INTERMEDIARIO/Assincrona/Exercícios  javaScript Assicronomo/Exercicio1.js
+++ JAVASCRIPT INTERMEDIARIO/Assincrona/Exercícios  javaScript Assicronomo/Exercicio1.js
@@ -1,2 +1,17 @@
 // Primeiro Exercício
 
+// Call back Simples
+
+// Escreva uma função imprimirMEnsagem que aceita uma mensagem e uma função 
+// de chamada de callBack. A função deve imprimir a mensagem no console e, em seguida, invocar a função d retorno de chamada
+
+
+function imprimirMensagem(mensagem, callback) {
+  console.log(mensagem);
+  callback("Mensagem impressa com sucesso!");
+}
+
+
+imprimirMensagem("Olá!", (resultado) => {
+  console.log("Callback:", resultado);
+});
